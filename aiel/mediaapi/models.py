61d722e7ea3f82a18957e64e4b8b768366f383fe
Your Name <you@example.com>
2025-01-16 21:31:58 +0200

    serializers

--- aiel/mediaapi/models.py
+++ aiel/mediaapi/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class MediaApi(models.Model):
+    id = models.AutoField(primary_key=True)
+    image = models.ImageField(upload_to='gallery_images/images/')
+    media_name = models.CharField(max_length=255)
+    caption = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        verbose_name = "Gallery"
+        verbose_name_plural = "Gallery"
+        ordering = ['created_at']
+    def __str__(self):
+        return self.media_name
+
--- aiel/team/serializers.py
+++ aiel/team/serializers.py
@@ -4,4 +4,4 @@ from .models import Team
 class TeamSerializer(serializers.ModelSerializer):
     class Meta:
         model = Team
-        fields = ['id', 'image', 'fullname', 'position', 'bio','created_at', 'updated_at']
+        fields = ['id', 'image', 'fullname', 'position', 'bio','linkedin_url','x_url','phone_number','email_address','created_at', 'updated_at']
